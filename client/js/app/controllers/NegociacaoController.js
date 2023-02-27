ef5ebfe3b63929cb486665a5f78097d2c9e4d883
Osvaldo Sousa <osvaldo.msn18@gmail.com>
2023-02-27 14:29:06 -0300

    create features clear and focus
    reser values input and focus for input date

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -5,11 +5,19 @@ class NegociacaoController {
     this._inputData = $('#data');
     this._inputQuantidade = $('#quantidade');
     this._inputValor = $('#valor');
-  }
+  };
+
+  limparComFocus() {
+    this._inputData.value = "";
+    this._inputData.focus()
+    this._inputQuantidade.value = 1;
+    this._inputValor.value = "0.0";
+  };
 
   adiciona(event) {
     event.preventDefault();
     const data = new Date(...this._inputData.value.split('-').map((item, indice) => item - indice % 2));
     const negociacao = new Negociacao(data, this._inputQuantidade.value, this._inputValor.value)
-  }
+    this.limparComFocus()
+  };
 }
